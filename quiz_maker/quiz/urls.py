978db6566b6188e763d1d1e261a03ff76dd0ad24
Sonerig <kambachokovidar@gmail.com>
2025-05-31 01:18:37 +0300

    Small fixes

--- quiz_maker/quiz/urls.py
+++ quiz_maker/quiz/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.upload_file, name="upload_file"),
-    path('start/', views.start_quiz, name="start_quiz"),
+    path('start/<int:question_id>/', views.start_quiz, name="start_quiz"),
     path('result/', views.result, name="result"),
 ]
