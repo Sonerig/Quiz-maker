de6f479c8118b03afffbf5196b5a572f47c2e7f2
Sonerig <77194357+Sonerig@users.noreply.github.com>
2025-06-02 12:14:20 +0300

    Fixed the problem: Session table doesn't exist in DB.

--- run_linux.sh
+++ run_linux.sh
@@ -3,7 +3,7 @@
 if [ ! -d venv ]; then
     python -m venv venv
     venv/bin/pip install -r requirements.txt
-    venv/bin/python quiz_maker/manager.py migrate
+    venv/bin/python quiz_maker/manage.py migrate
 fi
 source venv/bin/activate
 cd quiz_maker; python -m uvicorn quiz_maker.asgi:application --host 127.0.0.1 --port 8000
