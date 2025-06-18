71539ef282dd3b44c0c151afb1db17b8589e35e7
Sonerig <kambachokovidar@gmail.com>
2025-06-19 02:27:04 +0300

    Reset quiz app models

--- quiz_maker/quiz/models.py
+++ quiz_maker/quiz/models.py
@@ -1,7 +1,3 @@
 from django.db import models
-from django.conf import settings
 
 # Create your models here.
-class Class(models.Model):
-    class_name: models.CharField(max_length="5")
-    studentID: models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
