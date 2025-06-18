de170ce6edeede1c3f622c799847ae33993189f7
Sonerig <kambachokovidar@gmail.com>
2025-06-19 01:25:15 +0300

    Classes model added

--- quiz_maker/quiz/models.py
+++ quiz_maker/quiz/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Class(models.Model):
+    class_name: models.CharField(max_length="5")
+    studentID: models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
