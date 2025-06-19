23984d32e1b1a6ac43d708abe20be1192399f6bc
Sonerig <kambachokovidar@gmail.com>
2025-06-20 02:04:56 +0300

    Admin panel right fields view update

--- quiz_maker/users/admin.py
+++ quiz_maker/users/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Class, CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Пользовательские данные', {'fields': ('username', 'email', 'password', 'groups')}),
+        ('Персональные данные', {'fields': ('first_name', 'last_name', 'classID')}),
+        ('Статус', {'fields': ('is_active', 'is_staff')}),
+    )
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+        if request.user.is_superuser:
+            fieldsets += (
+                ('Администрирование', {
+                    'fields': ('is_superuser', 'user_permissions'),
+                }),
+            )
+        return fieldsets
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Class)
-admin.site.register(CustomUser)
