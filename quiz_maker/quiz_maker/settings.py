5dab5e97e10b576603110021685f0a7f4b1a5f59
Sonerig <kambachokovidar@gmail.com>
2025-06-19 03:07:15 +0300

    FIX THE AUTH_USER_MODEL!

--- quiz_maker/quiz_maker/settings.py
+++ quiz_maker/quiz_maker/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quiz'
+    'quiz',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
