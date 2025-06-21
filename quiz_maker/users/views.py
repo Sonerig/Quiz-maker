0e5149ba2cd589b48e3aa28ddeda42925204e2e2
Sonerig <kambachokovidar@gmail.com>
2025-06-21 21:21:07 +0300

    Auto redirect for different users groups

--- quiz_maker/users/views.py
+++ quiz_maker/users/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
-# Create your views here.
+
 
 def login_view(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return redirect('/admin')
+        elif request.user.is_staff:
+            return redirect('journal')
+        else:
+            return redirect('upload_file')
+
+    elif request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             login(request, form.get_user())
-            return redirect('upload_file')
+            if request.user.is_superuser:
+                return redirect('/admin')
+            elif request.user.is_staff:
+                return redirect('journal')
+            else:
+                return redirect('upload_file')
     else:
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form })
